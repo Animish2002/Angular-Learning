04a4ce222cadd0995fd3b1cedd41b255194259bf
Animish2002 <animishchopade123@gmail.com>
2025-11-03 20:00:41 +0530

    new project for code splitting

---
+++ component_templates/src/app/support-ticket/support-ticket.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-ticket',
+  standalone: false,
+  templateUrl: './support-ticket.html',
+  styleUrl: './support-ticket.css',
+})
+export class SupportTicket {
+
+}
---
+++ component_templates/src/app/traffic/traffic.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { dummyTrafficData } from '../../data/TrafficData';
+
+@Component({
+  selector: 'app-traffic',
+  standalone: false,
+  templateUrl: './traffic.html',
+  styleUrl: './traffic.css',
+})
+export class Traffic {
+  dummyTrafficData = dummyTrafficData
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
+}
