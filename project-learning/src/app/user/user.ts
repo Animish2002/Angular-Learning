000d2c3f249282ac94bd0624b7210d5043e7911d
Animish2002 <animishchopade123@gmail.com>
2025-11-03 12:59:22 +0530

    new project learning

--- project-learning/src/app/user/user.ts
+++ project-learning/src/app/user/user.ts
@@ -1,13 +1,12 @@
 import { Component ,EventEmitter ,Input, Output} from '@angular/core';
 
-
-
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
+
 export class User {
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
